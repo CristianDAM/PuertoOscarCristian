0dcbe8a7e5c4b29187bfaad38127d61f9dd8a589
Oscar Charro Rivera <oschariv@gmail.com>
2017-04-27 10:47:35 +0200

    Clases finalizadas

--- Alquiler.java
+++ Alquiler.java
@@ -1,22 +1,38 @@
 
 /**
  * Write a description of class Alquiler here.
- *
- * @author (your name)
+ * 
+ * @author (your name) 
  * @version (a version number or a date)
  */
 public class Alquiler
 {
     // instance variables - replace the example below with your own
-
+    private static int VALOR_MULTIPLICADOR_ESLORA = 10;
+    private static int VALOR_MULTIPLICADOR_BERNUA = 300;
+    private int diasOcupacion;
+    private int posicionAmarre;
+    private Barco barco;
 
     /**
      * Constructor for objects of class Alquiler
      */
-    public Alquiler()
+    public Alquiler(int numDias, Barco barco, int posicionAmarre)
     {
-
+        diasOcupacion = numDias;
+        this.barco = barco;
+        this.posicionAmarre = posicionAmarre;
     }
 
+    public String toString(){
+        String cadenaADevolver = "Numero de Dias de Alquiler: " + diasOcupacion + 
+            ".\nPosicion del Amarre: " + posicionAmarre + ".\nBarco Asociado:\n" 
+            + barco.toString();
+        return cadenaADevolver;
+    }
     
+    public float getPrecioAlquiler(){
+        float precio = (diasOcupacion * (VALOR_MULTIPLICADOR_ESLORA * (float)barco.getEslora())) + (VALOR_MULTIPLICADOR_BERNUA * (float)barco.getCoeficienteBernua());
+        return precio;
+    }
 }
--- Puerto.java
+++ Puerto.java
@@ -1,21 +1,77 @@
 
 /**
  * Write a description of class Puerto here.
- *
- * @author (your name)
+ * 
+ * @author (your name) 
  * @version (a version number or a date)
  */
 public class Puerto
 {
     // instance variables - replace the example below with your own
-    
+    private Alquiler[] amarres;
 
     /**
      * Constructor for objects of class Puerto
      */
     public Puerto()
     {
-        // initialise instance variables
-
+        amarres = new Alquiler[4];
     }
+
+    public double alquilarAmarre(int numDias, Barco barco){
+        int primerLibre = this.posicionPrimerAmarreLibre();
+        double precioAlquiler = -1;
+        Alquiler nuevoAlquiler = new Alquiler(numDias, barco, primerLibre);
+        if (primerLibre != -1) {
+            amarres[primerLibre] = nuevoAlquiler;
+            precioAlquiler = nuevoAlquiler.getPrecioAlquiler();
+        }
+        return precioAlquiler;
+    }
+
+    public float liquidarAlquilerAmarre(int posicionAmarre){
+        float precioAlquiler = -1;
+        if (posicionAmarre < amarres.length) {
+            if (amarres[posicionAmarre] != null) {
+                precioAlquiler = amarres[posicionAmarre].getPrecioAlquiler();
+                amarres[posicionAmarre] = null;
+            }
+        }
+
+        return precioAlquiler;
+    }
+
+    public int posicionPrimerAmarreLibre(){
+        int libre = -1;
+        boolean localizado = false;
+        for (int i = 0; i < amarres.length; i++){
+            if (amarres[i] == null && !localizado){
+                libre = i;
+                localizado = true;
+            }
+        }
+        return libre;
+    }
+
+    public boolean hayAmarresLibres(){
+        Boolean libre = false;
+        for (int i = 0; i < amarres.length; i++){
+            if (amarres[i].equals(null)){
+                libre = true;
+            }
+        }
+        return libre;
+    }
+
+    public void verEstadoAmarres(){
+        for (int i = 0; i < amarres.length; i++){
+            if (amarres[i] == null){
+                System.out.println("Amarre " + i + " vacio.");
+            }
+            else{
+                System.out.println(amarres[i].toString());
+            }
+        }
+    }
+
 }

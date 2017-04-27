1ab9ea9baa12720535edb9500f9aec7a3f6c04a0
Oscar Charro Rivera <oschariv@gmail.com>
2017-04-27 22:49:29 +0200

    mejoras y añadidos comentarios

--- Alquiler.java
+++ Alquiler.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class Alquiler here.
+ * La clase Alquiler sirve para crear objetos donde almacenar todos los datos
+ * correspondientes a los alquileres.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Oscar Charro Rivera (oschariv)
+ * @version 27-04-2017
  */
 public class Alquiler
 {
@@ -15,7 +16,10 @@ public class Alquiler
     private Barco barco;
 
     /**
-     * Constructor for objects of class Alquiler
+     * Constructor de la clase Alquiler
+     * @param numDias int para contabilizar los dias de alquiler
+     * @param barco objeto de la clase Barco a asociar el alquiler
+     * @param posicionAmarre int para posicion del amarre a alquilar
      */
     public Alquiler(int numDias, Barco barco, int posicionAmarre)
     {
@@ -24,15 +28,35 @@ public class Alquiler
         this.posicionAmarre = posicionAmarre;
     }
 
+    /**
+     * Metodo toString para imprime todo la informacion referente al alquiler
+     * @return cadenaADevolver - String para retorno de la informacion del alquiler
+     */
     public String toString(){
         String cadenaADevolver = "Numero de Dias de Alquiler: " + diasOcupacion + 
             ".\nPosicion del Amarre: " + posicionAmarre + ".\nBarco Asociado:\n" 
-            + barco.toString();
+            + barco.toString() + "\n";
         return cadenaADevolver;
     }
     
+    /**
+     * Metodo getPrecioAlquiler devuelve el precio calculado a partir de una formula.
+     * El precio del alquiler a pagar se calcula a partir del 
+     * número de días de ocupación multiplicado por un valor en función del barco 
+     * (esta valor es el resultante de multiplicar por 10 los metros de eslora). Al 
+     * resultado se le suma la cantidad obtenida de multiplicar un valor ﬁjo 
+     * (300 euros) por el coeficiente de Bernua del barco.
+     * El coeficiente de Bernua se calcula en función del tipo de barco:
+     *          El número de mástiles para veleros,
+     *          La potencia en CV para embarcaciones deportivas a motor,
+     *          La potencia en CV más número de camarotes para embarcaciones 
+     *          deportivas a motor de lujo (yates).
+     * @return precio - float para el devolver el precio de alquiler
+     */
     public float getPrecioAlquiler(){
-        float precio = (diasOcupacion * (VALOR_MULTIPLICADOR_ESLORA * (float)barco.getEslora())) + (VALOR_MULTIPLICADOR_BERNUA * (float)barco.getCoeficienteBernua());
+        float precio = (diasOcupacion * (VALOR_MULTIPLICADOR_ESLORA * 
+                       (float)barco.getEslora())) + (VALOR_MULTIPLICADOR_BERNUA * 
+                       (float)barco.getCoeficienteBernua());
         return precio;
     }
 }
--- Puerto.java
+++ Puerto.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class Puerto here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Esta clase Puerto se encargara de gestionar todo lo referente a los alquileres
+ * de los amarres.
+ * 
+ * @author Oscar Charro Rivera (oschariv) 
+ * @version 27-04-2017
  */
 public class Puerto
 {
@@ -11,24 +12,42 @@ public class Puerto
     private Alquiler[] amarres;
 
     /**
-     * Constructor for objects of class Puerto
+     * Constructor de la clase Puerto
      */
     public Puerto()
     {
         amarres = new Alquiler[4];
     }
-
+    
+    /**
+     * El metodo alquilarAmarre nos permite crear un objeto de la clase Alquiler
+     * que asociamos a la primera posicion libre en el array amarres.
+     * Devuelve -1 como error si no hay amarres libres, sino devuelve el precio de 
+     * alquiler.
+     * @param numDias int para señalar numero de dias que se va a alquilar el amarre
+     * @param barco objeto Barco al que se le va a alquilar el amarre
+     * @return precioAlquiler - int que señala el precio de alquiler o error
+     */
     public float alquilarAmarre(int numDias, Barco barco){
-        int primerLibre = this.posicionPrimerAmarreLibre();
         float precioAlquiler = -1;
-        Alquiler nuevoAlquiler = new Alquiler(numDias, barco, primerLibre);
-        if (primerLibre != -1) {
+        if (hayAmarresLibres()) {
+            int primerLibre = this.posicionPrimerAmarreLibre();
+            Alquiler nuevoAlquiler = new Alquiler(numDias, barco, primerLibre);
             amarres[primerLibre] = nuevoAlquiler;
             precioAlquiler = nuevoAlquiler.getPrecioAlquiler();
         }
         return precioAlquiler;
     }
 
+    /**
+     * El metodo liquidarAlquilerAmarre nos permite comprobar si el amarre indicado
+     * esta alquilado o no.
+     * Si esta alquilado obtiene el precio del alquiler para luego devolverlo y
+     *  borra el amarre, sino devuelve -1 como error de que ese amarre no esta
+     *  alquilado.
+     *  @param posicionAmarre int que señala la posicion del amarre
+     *  @return precioAlquiler - float que devuelve el precio de alquiler o error
+     */
     public float liquidarAlquilerAmarre(int posicionAmarre){
         float precioAlquiler = -1;
         if (posicionAmarre < amarres.length) {
@@ -40,7 +59,16 @@ public class Puerto
 
         return precioAlquiler;
     }
-
+    
+    /**
+     * El metodo posicionPrimerAmarreLibre nos localiza la primera posicion libre en
+     * el array.
+     * Si localiza una posicion libre en el array devuelve la posicion del array que 
+     * esta vacio y sino devuelver -1 como error de que esta lleno.
+     * Se usa un booleano para que si se localiza una posicion libre no se tenga que
+     * recorrer más el array.
+     * @return libre - int para posicion del primer amarre libre
+     */
     public int posicionPrimerAmarreLibre(){
         int libre = -1;
         boolean localizado = false;
@@ -53,16 +81,31 @@ public class Puerto
         return libre;
     }
 
+    /**
+     * El metodo hayAmarresLibres nos localiza si el array tiene posiciones libres.
+     * Si localiza una posicion libre en el array devuelve true y sino devuelve false.
+     * Se usa un booleano para que si se localiza una posicion libre no se tenga que
+     * recorrer más el array.
+     * @return libre - booleano que señala si hay huecos libres o no
+     */
     public boolean hayAmarresLibres(){
         Boolean libre = false;
+        boolean localizado = false;
         for (int i = 0; i < amarres.length; i++){
-            if (amarres[i].equals(null)){
+            if (amarres[i] == null && !localizado){
                 libre = true;
+                localizado = true;
             }
         }
         return libre;
     }
 
+    /**
+     * El metodo verEstadoAmarres nos permite imprimir por la terminal el estado del
+     * array.
+     * Si una posicion esta vacia se imprime un mensaje indicando que esa poscion esta
+     * vacia, y sino se imprimen todos los datos asociados al array en esa posicion.
+     */
     public void verEstadoAmarres(){
         for (int i = 0; i < amarres.length; i++){
             if (amarres[i] == null){
